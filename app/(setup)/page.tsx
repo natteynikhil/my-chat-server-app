46845c40d3642ef67432193903213c0be9f9039e
natteynikhil <natteynikhil41@gmail.com>
2024-03-14 23:59:27 +0530

    final

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,37 @@
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
+
+const SetupPage = async () => {
+    const profile = await initialProfile();
+    
+    const server = await db.server.findFirst({
+        where : {
+            members : {
+                some : {
+                    profileId : profile.id
+                }
+            }
+        }
+    });
+
+    if(server){
+        console.log("NIKHIL",server.id)
+        return redirect(`/servers/${server.id}`);
+    }
+
+
+
+    return ( 
+        <div>
+            <InitialModal/>
+        </div>
+     );
+}
+ 
+export default SetupPage;
+
+
+
+
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,52 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains : [
+//       "utfs.io"
+//     ]
+//   }
+// };
+
+// export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns : [
+//       {
+//         protocol: "https",
+//         hostname: "utfs.io",
+//       }
+      
+//     ]
+//   }
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil"
+    });
+
+    return config;
+  },
+  images: {
+    remotePatterns : [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      }
+      
+    ]
+  }
+};
 
 export default nextConfig;
+
+
